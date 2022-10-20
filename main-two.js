f2be38a4890191ce977b5380af2afceaf446f96f
Michael <michaelmccracken1985@gmail.com>
2022-10-20 18:30:21 -0400

    Commit "main-add.js" `getComputerChoice` Works And Returns Value to `computerSelection`.

--- main-two.js
+++ main-two.js
@@ -1,11 +1,17 @@
 // Return results, don't console.log()
 
 // Return random "Rock" "Paper" "Scissors"
+let computerSelection;
+
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
-  let computerSelection = choices[Math.floor(Math.random() * choices.length)];
-  console.log(computerSelection);
+  computerSelection = choices[Math.floor(Math.random() * choices.length)];
+  return computerSelection;
 }
+
+// Must call getComputerChoice() for computerSelection to be logged
+getComputerChoice();
+console.log(computerSelection);
 
 // function playRound(playerSelection, computerSelection) {
 // your code here!
