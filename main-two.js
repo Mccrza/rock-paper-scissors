d101a5283e8624deb04e5e6c6d053dbf9433ac96
Michael <michaelmccracken1985@gmail.com>
2022-10-21 00:06:51 -0400

    Previous Commit Msg Meant for this Commit of "main-two.js". Write Definitions for Variables `getPlayerSelection` and `getComputerSelection` Using Respective Functions, `getPlayerChoice` and `getComputerChoice`. Write Rough Draft for `game()` Function.

--- main-two.js
+++ main-two.js
@@ -1,29 +1,38 @@
 // Return results, don't console.log()
 
 // Return random "Rock" "Paper" "Scissors"
-let computerSelection;
+let computerSelection = (function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"];
+    let random = choices[Math.floor(Math.random() * choices.length)];
+    return random;
+})();
 
-function getComputerChoice() {
-  let choices = ["rock", "paper", "scissors"];
-  computerSelection = choices[Math.floor(Math.random() * choices.length)];
-  return computerSelection;
-}
+// Prompt player to enter their move, stores move in variable
+let playerSelection = (function getPlayerChoice() {
+    let playerPrompt = prompt("What is your move?");
+    let playerPromptInput = playerPrompt.toLowerCase();
+    return playerPromptInput;
+})();
 
-// Must call getComputerChoice() for computerSelection to be logged
-getComputerChoice();
-console.log(computerSelection);
-
-let playerPrompt = prompt("What is your move?");
-let playerSelection = playerPrompt.toLowerCase();
-
-function playRound(playerSelection, computerSelection) {}
-
+// Play one round, evaluate results & declare winner, repeat x5
 function game() {
   for (let i = 0; i < 5; i++) {
-    playRound();
+    swtich (playRound(playerSelection, computerSelection)) {
+        case (playerSelection === "rock" && computerSelection === "scissors"):
+        case (playerSelection === "paper" && computerSelection === "rock"):
+        case (playerSelection === "scissors" && computerSelection === "paper"):
+            alert(`You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).}.`);
+            break;
+        case (playerSelection === "rock" && computerSelection === "paper"):
+        case (playerSelection === "paper" && computerSelection === "scissors"):
+        case (playerSelection === "scissors" && computerSelection === "rock"):
+            alert(`You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).}`);
+            break;
+        case (playerSelection === computerSelection):
+            alert(`The result is a draw! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)`} ties ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).}`);
+    }
   }
 }
-
 // function playRound(playerSelection, computerSelection) {
 // your code here!
 // }
