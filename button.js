c426da0b0c19f89cd900483f612c091ce3b58d4d
Michael <michaelmccracken1985@gmail.com>
2022-10-22 23:46:54 -0400

    Delete "button.js" Unused Javascript

--- button.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById("btn");
-
-// ✅ Change button text on click
-btn.addEventListener("click", function handleClick() {
-  btn.textContent = "PLAY ACCEPTED AWAIT INSTRUCTION";
-});
