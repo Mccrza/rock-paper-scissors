3a0c18b59085602e24541209cb5a3165a01c447a
Michael <michaelmccracken1985@gmail.com>
2022-10-23 16:08:07 -0400

    Define Three Functions `getComputerChoice()` `getPlayerChoice()` and `playRound()`. Define const `playerSelection` and `computerSelection` with Respective Functions. Call `playRound()` With Arguments.

--- main-two.js
+++ main-two.js
@@ -1,35 +1,36 @@
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    function playRound(playerSelection, computerSelection) {
-      function getComputerChoice() {
-        let choices = ["rock", "paper", "scissors"];
-        let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-          
-          return computerChoice;
-      }  
-      let playerSelection = (function getPlayerChoice() {
-        let playerPrompt = prompt("Whatx is your move?");
-        let playerChoice = playerPrompt.toLowerCase();
-        return playerChoice;
-      })();
-         if (
-          (playerSelection === "rock" && computerSelection === "scissors") ||
-          (playerSelection === "paper" && computerSelection === "rock") ||
-          (playerSelection === "scissors" && computerSelection === "paper")
-        ) {
-          alert(`You win! ${playerSelection} beats ${computerSelection}.`);
-        } else if (
-          (playerSelection === "rock" && computerSelection === "paper") ||
-          (playerSelection === "paper" && computerSelection === "scissors") ||
-          (playerSelection === "scissors" && computerSelection === "rock")
-        ) {
-          alert(`You lose! ${computerSelection} beats ${playerSelection}.`);
-        } else if (playerSelection === computerSelection) {
-          alert(
-            `The result is a draw! ${playerSelection} ties ${computerSelection}.`
-          );
-        }
-      }
-    }
+function getComputerChoice() {
+  let choices = ["rock", "paper", "scissors"];
+  let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  return computerChoice;
+}
+
+function getPlayerChoice() {
+  let playerPrompt = prompt("What is your goddamned move");
+  let playerChoice = playerPrompt.toLowerCase();
+  return playerChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    alert(`You win! ${playerSelection} beats ${computerSelection}.`);
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    alert(`You lose! ${computerSelection} beats ${playerSelection}.`);
+  } else if (playerSelection === computerSelection) {
+    alert(
+      `The result is a draw! ${playerSelection} ties ${computerSelection}.`
+    );
   }
 }
+
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+
+playRound(playerSelection, computerSelection);
