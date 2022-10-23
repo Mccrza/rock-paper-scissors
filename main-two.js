d6418fceaf87394ca080790d81d2c6f857073aed
Michael <michaelmccracken1985@gmail.com>
2022-10-23 15:45:56 -0400

    Delete Functions `getPlayerChoice()` and `getComputerChoice()`

--- main-two.js
+++ main-two.js
@@ -1,25 +1,33 @@
-function game() {
+function playGame() {
   for (let i = 0; i < 5; i++) {
-    function playRound() {
+    function playRound(playerSelection, computerSelection) {
       function getComputerChoice() {
         let choices = ["rock", "paper", "scissors"];
         let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-        return computerChoice;
-      }
-      function getPlayerChoice() {
-        let playerPrompt = prompt("What is your move?");
+          
+          return computerChoice;
+      }  
+      let playerSelection = (function getPlayerChoice() {
+        let playerPrompt = prompt("Whatx is your move?");
         let playerChoice = playerPrompt.toLowerCase();
         return playerChoice;
-      }
-      let playerSelection = getPlayerChoice();
-      let computerSelection = getComputerChoice();
-      function playRound() {
-        if (playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || playerSelection === "scissors" && computerSelection === "paper") {
+      })();
+         if (
+          (playerSelection === "rock" && computerSelection === "scissors") ||
+          (playerSelection === "paper" && computerSelection === "rock") ||
+          (playerSelection === "scissors" && computerSelection === "paper")
+        ) {
           alert(`You win! ${playerSelection} beats ${computerSelection}.`);
-        } else if (playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors") || (playerSelection "scissors" && computerSelection === "rock" ) {
-          alert(`You lose! ${computerSelection} beats ${playerSelection}.`); 
+        } else if (
+          (playerSelection === "rock" && computerSelection === "paper") ||
+          (playerSelection === "paper" && computerSelection === "scissors") ||
+          (playerSelection === "scissors" && computerSelection === "rock")
+        ) {
+          alert(`You lose! ${computerSelection} beats ${playerSelection}.`);
         } else if (playerSelection === computerSelection) {
-          alert(`The result is a draw! ${playerSelection} ties ${computerSelection}.`)
+          alert(
+            `The result is a draw! ${playerSelection} ties ${computerSelection}.`
+          );
         }
       }
     }
