e6e89d4ac92abccf9ba2bf7615f36ecb75fe365d
Michael <michaelmccracken1985@gmail.com>
2022-10-23 20:39:11 -0400

    Define `game()` Function With Loop to Iterate Through 5 Rounds (`playRound()`). Add Statements `playerScore += 1` or `computerScore += 1` After Each Round Is Played. Define Function `compareScore()` to Judge Winner At End of a Game. Add Call to `compareScore()` After `playRound()`

--- main-two.js
+++ main-two.js
@@ -13,28 +13,68 @@ function getPlayerChoice() {
 function playRound(playerSelection, computerSelection) {
   if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
+    (playerSelection === "paper" && computerSelection === "rock")
   ) {
+    playerScore += 1;
     alert(
-      `You win! Your ${playerSelection.toUpperCase()} beats Master Control's ${computerSelection.toUpperCase()}.`
+      `You win! Your ${playerSelection.toUpperCase()} beats MasterControl's ${computerSelection.toUpperCase()}.`
+    );
+  } else if (playerSelection == "scissors" && computerSelection === "paper") {
+    playerScore += 1;
+    alert(
+      `You win! Your ${playerSelection.toUpperCase()} beat MasterControl's ${computerSelection.toUpperCase()}.`
     );
   } else if (
     (playerSelection === "rock" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "rock")
   ) {
+    computerScore += 1;
     alert(
-      `You lose! Master Control's ${computerSelection.toUpperCase()} beats your ${playerSelection.toUpperCase()}.`
+      `You lose! MasterControl's ${computerSelection.toUpperCase()} beats your ${playerSelection.toUpperCase()}.`
+    );
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore += 1;
+    alert(
+      `You lose! MasterControl's ${computerSelection.toUpperCase()} beat your ${playerSelection.toUpperCase()}.`
     );
   } else if (playerSelection === computerSelection) {
+    playerScore += 1;
+    computerScore += 1;
     alert(
-      `The result is a draw! Your ${playerSelection.toUpperCase()} ties with Master Control's ${computerSelection.toUpperCase()}.`
+      `The result is a draw! Your ${playerSelection.toUpperCase()} ties with MasterControl's ${computerSelection.toUpperCase()}.`
+    );
+  } else {
+    computerScore += 1;
+    alert(
+      `You lose! MasterControl's ${computerSelection.toUpperCase()} beats your ...${playerSelection.toUpperCase()}.`
     );
   }
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = getPlayerChoice();
+function game() {
+  {
+    for (i = 0; i < 5; i++) {
+      const computerSelection = getComputerChoice();
+      const playerSelection = getPlayerChoice();
+      playRound(playerSelection, computerSelection);
+    }
+    compareScore(playerScore, computerScore);
+  }
+}
 
-playRound(playerSelection, computerSelection);
+function compareScore() {
+  if (playerScore > computerScore) {
+    alert(
+      `Victory is yours! SCORE:  You - [ ${playerScore} ] pts vs MasterControl - [ ${computerScore} ] pts`
+    );
+  } else {
+    alert(
+      `You've suffered a crushing defeat... SCORE:  You - [ ${playerScore} ] pts vs MasterControl - [ ${computerScore} ] pts`
+    );
+  }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+game();
